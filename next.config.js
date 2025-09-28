562f45c0859f04e52e916b00c8280856ee0e7933
JITENDRA SINGH BISHT <152790288+vickybisht001@users.noreply.github.com>
2025-09-28 17:58:09 +0530

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,10 @@
+// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    optimizeCss: false, 
+    turbo: false,       
+  },
+};
+
+module.exports = nextConfig;
